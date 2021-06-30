d1b34a2b76a7b6a3239bbf6afae330958bbc3473
Smith Cruise <chendingchao1@126.com>
2021-06-30 12:53:16 +0800

    Create Uploader.java

---
+++ src/main/java/org/inlighting/sfm/uploader/Uploader.java
@@ -0,0 +1,41 @@
+package org.inlighting.sfm.uploader;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Uploader {
+    public static void main(String[] args) throws IOException {
+        String localFolder = "/home/smith/1w-articles/1w-articles";
+        String hdfsFolder = "/1w-articles.sfm";
+        Path path = new Path("sfm://master.lab.com:9000"+hdfsFolder);
+        FileSystem fs = path.getFileSystem(new Configuration());
+        File dir = new File(localFolder);
+        if (! dir.isDirectory()) {
+            return;
+        }
+        File[] files = dir.listFiles();
+        for (File file: files) {
+            Path tmp = new Path(hdfsFolder+"/"+file.getName());
+            FSDataOutputStream out = fs.create(tmp);
+            write(out, file);
+            out.close();
+        }
+        fs.close();
+    }
+
+    private static void write(FSDataOutputStream out, File file) throws IOException {
+        FileInputStream inputStream = new FileInputStream(file);
+        byte[] buffer = new byte[128];
+        int read;
+        while ((read = inputStream.read(buffer)) > 0) {
+            out.write(buffer, 0, read);
+        }
+    }
+}
